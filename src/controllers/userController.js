07f27e48c1ccc3d6c437559bc894ceb43a88dfa7
Erick Marcelo <marcerican@gmail.com>
2022-09-22 21:00:01 -0500

    Login user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,22 @@ import { SECRET } from "../config.js";
 import User from "../models/user.js"
 
 export const loginUser = async (req, res) => {
-  res.json({ user: "user auth" })
+  console.log(req.body)
+  try {
+    const { email, password } = req.body
+    if (!email || !password) return res.status(400).json({ message: "Missing fields" })
+
+    const userFound = await User.findOne({ email })
+    if (!userFound) return res.status(400).json({ message: "User not found" })
+
+    const isPasswordCorrect = await bcrypt.compare(password, userFound.password)
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" })
+
+    const token = jwt.sign({ id: userFound._id }, SECRET, { expiresIn: "1h" })
+    return res.status(200).json({ data: { _id: userFound._id, name: userFound.name, avatar: userFound.avatar }, token })
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 
 }
 
@@ -24,7 +39,6 @@ export const registerUser = async (req, res) => {
     if (!user) return res.status(409).json({ message: "User not created" })
 
     return res.status(201).json({ data: { _id: user._id, name: user.name, avatar: user.avatar }, token })
-
   } catch (error) {
     return res.status(500).json({ message: error.message }) // TODO Improve this (Invalid Email !== Internal Error)
   }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,6 +3,7 @@ import { loginUser, registerUser } from "../controllers/userController.js";
 
 const router = Router();
 
-router.post("/", registerUser)
+router.post("/register", registerUser)
+router.post("/login", loginUser)
 
 export default router;
