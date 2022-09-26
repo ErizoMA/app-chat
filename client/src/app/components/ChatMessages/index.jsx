fb74961b2ee5a03b4d23e3f0d64032454a750ba0
Erick Marcelo <marcerican@gmail.com>
2022-09-25 19:53:06 -0500

    Create ChatMessages component

--- client/src/app/components/ChatMessages/index.jsx
+++ client/src/app/components/ChatMessages/index.jsx
@@ -3,17 +3,21 @@ import styles from './index.module.css'
 function ChatMessages () {
   return (
     <div className={styles.container}>
-      <section>
-        <div>
-          Messages are end-to-end encrypted. No one outside of this chat, not even WhatsApp can read or listen to them click to learn more.
-        </div>
-        <div>
-          SingleMessage
-        </div>
-      </section>
-      <section>
-        Footer
-      </section>
+      <div className={styles.encrypted}>
+        Messages are end-to-end encrypted. No one outside of this chat, not even <br /> WhatsApp can read or listen to them click to learn more.
+      </div>
+      <div className={styles.message}>
+        Here are some photos. you were asking about.
+      </div>
+      <div className={styles.message__right}>
+        Here are some photos. you were asking about.
+      </div>
+      <div className={styles.message}>
+        Here are some photos. you were asking about.
+      </div>
+      <div className={styles.message}>
+        Here are some photos. you were asking about.
+      </div>
     </div>
   )
 }
