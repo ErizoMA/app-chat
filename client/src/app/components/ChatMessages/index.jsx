39cdf18fa8b2c9f24d5c894f1933e8e55a3a4a19
Erick Marcelo <marcerican@gmail.com>
2022-09-25 19:59:15 -0500

    Fix ChatBody interface

--- client/src/app/components/ChatMessages/index.jsx
+++ client/src/app/components/ChatMessages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LockIcon from '../Icons/Lock'
 import styles from './index.module.css'
 function ChatMessages () {
   return (
     <div className={styles.container}>
       <div className={styles.encrypted}>
+        <LockIcon />
         Messages are end-to-end encrypted. No one outside of this chat, not even <br /> WhatsApp can read or listen to them click to learn more.
       </div>
       <div className={styles.message}>
@@ -18,6 +20,10 @@ function ChatMessages () {
       <div className={styles.message}>
         Here are some photos. you were asking about.
       </div>
+      <div className={styles.message}>
+        Here are some photos. you were asking about.
+      </div>
+
     </div>
   )
 }
---
+++ client/src/app/components/Icons/Lock.jsx
@@ -0,0 +1,34 @@
+const LockIcon = (props) => (
+  <svg
+    width={24}
+    height={24}
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M16.424 9.448V7.3a4.552 4.552 0 0 0-4.551-4.551 4.55 4.55 0 0 0-4.57 4.53v2.168'
+      stroke='#312E40'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      clipRule='evenodd'
+      d='M15.683 21.25h-7.64a3.792 3.792 0 0 1-3.793-3.793v-4.289a3.792 3.792 0 0 1 3.792-3.792h7.641a3.792 3.792 0 0 1 3.792 3.793v4.288a3.792 3.792 0 0 1-3.792 3.793Z'
+      stroke='#312E40'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M11.863 14.203v2.22'
+      stroke='#312E40'
+      strokeWidth={1.5}
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+
+export default LockIcon
